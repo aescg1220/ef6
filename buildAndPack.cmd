a903f57c99dce53378d654a0e151f9d4ff2a2133
Cody Gentry <Cody.Gentry@aeshield.com>
2023-04-28 09:06:24 -0400

    changed version suffix in buildAndPack to unofficial

--- buildAndPack.cmd
+++ buildAndPack.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -c Release -sign -pack -restore -build /p:VersionSuffix=timeouthotfix %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -c Release -sign -pack -restore -build /p:VersionSuffix=unofficial %*"
 exit /b %ErrorLevel%
